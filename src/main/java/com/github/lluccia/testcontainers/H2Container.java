74f14a21973ed25eb2da02c698aa2f76a866d726
Leandro Conca <lluccia@gmail.com>
2023-11-23 00:01:30 -0300

    starts h2 in tcp mode and create empty database

--- src/main/java/com/github/lluccia/testcontainers/H2Container.java
+++ src/main/java/com/github/lluccia/testcontainers/H2Container.java
@@ -1,15 +1,49 @@
 package com.github.lluccia.testcontainers;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.MountableFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class H2Container extends GenericContainer<H2Container> {
 
     private static final String IMAGE_NAME = "eclipse-temurin:21-jre-alpine";
+    private static final String H2_JAR_PATH = "/h2/h2.jar";
 
     public H2Container() {
         super(IMAGE_NAME);
 
-        withCommand("sleep 1d");
+        String homeDir = System.getenv("HOME");
+
+        withCopyFileToContainer(
+                MountableFile.forHostPath(
+                        Path.of(homeDir, ".m2/repository/com/h2database/h2/2.1.214/h2-2.1.214.jar"))
+                , H2_JAR_PATH);
+
+        withCopyFileToContainer(
+                MountableFile.forClasspathResource("init.sql"), "/h2/init.sql");
+
+        withCommand("/opt/java/openjdk/bin/java",
+                "-cp", H2_JAR_PATH,
+                "org.h2.tools.Server",
+                "-tcp", "-tcpAllowOthers",
+                "-web", "-webAllowOthers"
+        );
+    }
+
+    @Override
+    protected void containerIsStarted(InspectContainerResponse containerInfo) {
+        try {
+            execInContainer("/opt/java/openjdk/bin/java",
+                    "-cp", H2_JAR_PATH,
+                    "org.h2.tools.RunScript",
+                    "-url", "jdbc:h2:~/test;DB_CLOSE_DELAY=-1",
+                    "-script", "/h2/init.sql"
+            );
+        } catch (IOException|InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/github/lluccia/testcontainers/H2ContainerTest.java
+++ src/test/java/com/github/lluccia/testcontainers/H2ContainerTest.java
@@ -1,12 +1,13 @@
 package com.github.lluccia.testcontainers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,16 +17,14 @@ class H2ContainerTest {
     private static Logger logger = LoggerFactory.getLogger("tc.h2");
 
     @Container
-    public H2Container h2Container = new H2Container();
-
-    @BeforeEach
-    void setup() {
-        h2Container.withLogConsumer(new Slf4jLogConsumer(logger));
-    }
+    public H2Container h2Container = new H2Container()
+            .withExposedPorts(8082, 9092)
+            .withLogConsumer(new Slf4jLogConsumer(logger));
 
     @Test
-    void canCreateGenericContainer() {
+    void canCreateContainer() {
         assertTrue(h2Container.isCreated());
     }
+
 }
 
